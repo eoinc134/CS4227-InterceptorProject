30e5ea81f298d34fe9caa226ecade16bd09f01f6
eoinc134 <60822259+eoinc134@users.noreply.github.com>
2023-02-22 14:16:15 +0000

    Automatic Testing fixed

--- Data/app.py
+++ Data/app.py
@@ -1,10 +1,11 @@
 import os, sys
-from WeatherData import WeatherData
 
 currDir = os.path.dirname(os.path.realpath(__file__))
 rootDir = os.path.abspath(os.path.join(currDir, '..'))
 if rootDir not in sys.path:  # add parent dir to paths
     sys.path.append(rootDir)
+
+from Data.WeatherData import WeatherData
 
 from Display.CurrentConditionsDisplay import CurrentConditionsDisplay 
 from Display.StatisticsDisplay import StatisticsDisplay 
